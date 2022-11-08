58d3ab703195e4f72c506eb017d0e2bf7d0b38b6
Nathan <nathanchite@gmail.com>
2022-11-08 19:40:58 -0300

    rr

---
+++ src/Cliente.java
@@ -0,0 +1,42 @@
+public class Cliente {
+    
+    String nome;
+    String conta;
+    String agenciaString;
+    float saldo;
+    
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getConta() {
+        return conta;
+    }
+
+    public void setConta(String conta) {
+        this.conta = conta;
+    }
+
+    public String getAgenciaString() {
+        return agenciaString;
+    }
+
+    public void setAgenciaString(String agenciaString) {
+        this.agenciaString = agenciaString;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+    
+    
+    
+    
+}
---
+++ src/main.java
@@ -0,0 +1,29 @@
+import java.sql.ResultSet;
+
+public class main {
+    
+public static void main(String[] args) {
+    
+    Teste con = new Teste();
+    String sql = "Select * from Cadastro";
+    
+    ResultSet rs = con.executaBusca(sql);
+
+    try {
+        while(rs.next()){
+            String nome = rs.getString("Nome");
+            String conta = rs.getString("Conta");
+            String agencia = rs.getString("Agencia");
+            float saldo = rs.getFloat("Saldo");
+            System.out.println(nome + " - " + conta + " - " + agencia + " - " + saldo );
+        }
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+
+    
+
+}
+
+
+}
